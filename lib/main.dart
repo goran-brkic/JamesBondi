0066f0dcfa086034ba85c39e2f4ac52b09a98790
Marin Fabijanic <mf51426@fer.hr>
2020-10-09 09:08:29 +0200

    Added sign in/out

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'signup.dart';
+import 'signin.dart';
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
 void main() {
   runApp(App());
 }
@@ -81,12 +82,12 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _signed = false;
 
   void signOutButton() async {
-    final User user = _auth.currentUser;
+    final User user = auth.currentUser;
     if (user == null) {
       _signed = false;
       return;
     }
-    await _auth.signOut();
+    await auth.signOut();
     final String uid = user.uid;
     _signed = false;
     Scaffold.of(context).showSnackBar(SnackBar(
@@ -101,22 +102,30 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Builder(builder: (BuildContext context) {
-        return Container(
-          child: Column(
-            children: <Widget>[
-              Text(
-                'You are signed ',
-              ),
-              Text(
-                _signed ? "IN" : "OUT",
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Container(
-                height: 300,
-                child: _RegisterEmailSection(),
-              ),
-            ],
-          ),
+        return ListView(
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.all(16),
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  'You are signed ',
+                ),
+                Text(
+                  _signed ? "IN" : "OUT",
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Container(
+                  height: 300,
+                  child: RegisterEmailSection(),
+                ),
+                Container(
+                  height: 300,
+                  child: EmailPasswordForm(),
+                ),
+              ],
+            ),
+          ],
         );
       }),
       floatingActionButton: FloatingActionButton(
@@ -124,102 +133,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class _RegisterEmailSection extends StatefulWidget {
-  final String title = 'Registration';
-  @override
-  State<StatefulWidget> createState() => _RegisterEmailSectionState();
-}
-
-class _RegisterEmailSectionState extends State<_RegisterEmailSection> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _success;
-  String _userEmail;
-  void _register() async {
-    final User user = (await _auth.createUserWithEmailAndPassword(
-      email: _emailController.text,
-      password: _passwordController.text,
-    ))
-        .user;
-    if (user != null) {
-      setState(() {
-        _success = true;
-        _userEmail = user.email;
-        user.sendEmailVerification();
-      });
-    } else {
-      setState(() {
-        _success = true;
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              alignment: Alignment.center,
-              child: RaisedButton(
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    _register();
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: Text(_success == null
-                  ? ''
-                  : (_success
-                      ? 'Successfully registered ' + _userEmail
-                      : 'Registration failed')),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
