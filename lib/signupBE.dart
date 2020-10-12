cdfa54219f4e5ac2e282299da4481c43aeff78cd
Marin Fabijanic <mf51426@fer.hr>
2020-10-12 18:50:38 +0200

    Dodan user info

---
+++ lib/signupBE.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void addLecturer(final String username, final String mail, final DateTime dob,
+    final String firstName, final String lastName, final String iban) {
+  FirebaseFirestore.instance
+      .collection('users')
+      .add({
+        'username': username,
+        'mail': mail,
+        'dob': dob,
+        'firstName': firstName,
+        'lastName': lastName,
+        'iban': iban,
+        'lecturer': true
+      })
+      .then((value) => print("User (lecturer) Added"))
+      .catchError((error) => print("Failed to add user: $error"));
+}
+
+void addStudent(
+    final String username,
+    final String mail,
+    final DateTime dob,
+    final String firstName,
+    final String lastName,
+    final String creditCard,
+    final DateTime cardExp,
+    final int secCode) {
+  FirebaseFirestore.instance
+      .collection('users')
+      .add({
+        'username': username,
+        'mail': mail,
+        'dob': dob,
+        'firstName': firstName,
+        'lastName': lastName,
+        'creditCard': creditCard,
+        'cardExp': cardExp,
+        'secCode': secCode,
+        'lecturer': false
+      })
+      .then((value) => print("User (student) Added"))
+      .catchError((error) => print("Failed to add user: $error"));
+}
