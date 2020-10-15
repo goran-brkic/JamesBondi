5d5d898eba94b77b71d467e0ecfc1baa3dad5d4b
Goran Brkic <gb51908@fer.hr>
2020-10-15 15:04:17 +0200

    Novi raspored i welcome page

--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:jamesbondi/Screens/Welcome/components/body.dart';
 
 class WelcomeScreen extends StatelessWidget {
