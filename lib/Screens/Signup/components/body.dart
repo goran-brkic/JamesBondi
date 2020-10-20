54a190c77484343b57c771d76eae451f1a1fb156
Marin Fabijanic <mf51426@fer.hr>
2020-10-20 16:25:34 +0000

    Update body.dart

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:jamesbondi/Screens/Welcome/welcome_screen.dart';
 import 'package:jamesbondi/components/InputField.dart';
+import 'package:jamesbondi/components/roundedButton.dart';
 import 'package:jamesbondi/constants.dart';
 
 final FirebaseAuth auth = FirebaseAuth.instance;
@@ -10,27 +12,72 @@ bool signed = false;
 enum Person { lecturer, student }
 
 class Body extends StatefulWidget {
+  String emailAddressInput;
+  String usernameInput;
+
+  Body({@required this.emailAddressInput, @required this.usernameInput});
+
   @override
   _Body createState() => _Body();
 }
 
 class _Body extends State<Body> {
-  String firstName;
-  String lastName;
-  String email;
-  String username;
-  String password;
-  String creditcard;
   bool lecturer = true;
   Person _radioValue1 = Person.lecturer;
-  String secCode;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _creditcardController = TextEditingController();
+  final TextEditingController _expirationDateController =
+      TextEditingController();
+  final TextEditingController _secCodeController = TextEditingController();
+  final TextEditingController _ibanController = TextEditingController();
+  final TextEditingController _aboutYController = TextEditingController();
+  bool _success;
+  String _userEmail;
+
+  void _register() async {
+    final User user = (await auth.createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+    if (user != null) {
+      setState(() {
+        signed = true;
+        _success = true;
+        _userEmail = user.email;
+        user.sendEmailVerification();
+//      addLecturer('test', user.email, null, 'Testno', 'Ime', 'Moneyyyy');
+      });
+    } else {
+      setState(() {
+        _success = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _emailController.text = widget.emailAddressInput;
+    _usernameController.text = widget.usernameInput;
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
+        body: Form(
+      key: _formKey,
+      child: SingleChildScrollView(
         child: Container(
-          height: size.height * 1.1,
+          height: size.height * (lecturer ? 1.6 : 1.15),
           width: double.infinity,
           child: Stack(
             alignment: Alignment.center,
@@ -60,10 +107,26 @@ class _Body extends State<Body> {
                 child: Row(
                   children: <Widget>[
                     Padding(
-                      padding: EdgeInsets.all(1),
-                      child: Text('Are you a lecturer or student?'),
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        'Are you a lecturer or student?',
+                        style: TextStyle(
+                          fontFamily: 'RoundLight',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15,
+                          color: customPurple,
+                        ),
+                      ),
                     ),
-                    Text('Lecturer'),
+                    Text(
+                      'Lecturer',
+                      style: TextStyle(
+                        fontFamily: 'RoundLight',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15,
+                        color: Colors.grey[800],
+                      ),
+                    ),
                     Radio(
                         value: Person.lecturer,
                         groupValue: _radioValue1,
@@ -73,7 +136,15 @@ class _Body extends State<Body> {
                             _radioValue1 = value;
                           });
                         }),
-                    Text('Student'),
+                    Text(
+                      'Student',
+                      style: TextStyle(
+                        fontFamily: 'RoundLight',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15,
+                        color: Colors.grey[800],
+                      ),
+                    ),
                     Radio(
                         value: Person.student,
                         groupValue: _radioValue1,
@@ -86,13 +157,43 @@ class _Body extends State<Body> {
                   ],
                 ),
               ),
-              InputField(title: "First name", topValue: 0.25),
-              InputField(title: "Last name", topValue: 0.37),
-              InputField(title: "Email", topValue: 0.49),
-              InputField(title: "Username", topValue: 0.61),
-              InputField(title: "Password", topValue: 0.73),
+              InputField(
+                title: "First name",
+                topValue: 0.25,
+                controller: _firstNameController,
+                enabledField: true,
+              ),
+              InputField(
+                title: "Last name",
+                topValue: 0.37,
+                controller: _lastNameController,
+                enabledField: true,
+              ),
+              InputField(
+                title: "Email",
+                topValue: 0.49,
+                controller: _emailController,
+                enabledField: false,
+              ),
+              InputField(
+                title: "Username",
+                topValue: 0.61,
+                controller: _usernameController,
+                enabledField: false,
+              ),
+              InputField(
+                title: "Password",
+                topValue: 0.73,
+                controller: _passwordController,
+                enabledField: true,
+              ),
               if (!lecturer) ...[
-                InputField(title: "Credit card", topValue: 0.85),
+                InputField(
+                  title: "Credit card",
+                  topValue: 0.85,
+                  controller: _creditcardController,
+                  enabledField: true,
+                ),
                 Positioned(
                   top: size.height * 0.97,
                   left: size.width * 0.12,
@@ -105,7 +206,8 @@ class _Body extends State<Body> {
                       borderRadius: BorderRadius.circular(29),
                       border: Border.all(color: customPurple),
                     ),
-                    child: TextField(
+                    child: TextFormField(
+                      controller: _expirationDateController,
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: "MM/YY",
@@ -132,7 +234,7 @@ class _Body extends State<Body> {
                       fontFamily: 'RoundLight',
                       fontWeight: FontWeight.normal,
                       fontSize: 15,
-                      color: customPurple,
+                      color: Colors.grey[800],
                     ),
                   ),
                 ),
@@ -148,7 +250,8 @@ class _Body extends State<Body> {
                       borderRadius: BorderRadius.circular(29),
                       border: Border.all(color: customPurple),
                     ),
-                    child: TextField(
+                    child: TextFormField(
+                      controller: _secCodeController,
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: "XXX",
@@ -175,15 +278,143 @@ class _Body extends State<Body> {
                       fontFamily: 'RoundLight',
                       fontWeight: FontWeight.normal,
                       fontSize: 15,
-                      color: customPurple,
+                      color: Colors.grey[800],
                     ),
                   ),
+                ),
+                Positioned(
+                  top: size.height * 1.05,
+                  child: FlatButton(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10, horizontal: size.width * 0.07),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => WelcomeScreen()));
+                      },
+                      color: customPurple,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text(
+                        "Create Account",
+                        style: TextStyle(
+                            fontFamily: 'RoundLight',
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20,
+                            color: Colors.white),
+                      )),
+                ),
+              ],
+              if (lecturer) ...[
+                InputField(
+                  title: "IBAN",
+                  topValue: 0.85,
+                  controller: _ibanController,
+                  enabledField: true,
+                ),
+                Positioned(
+                  left: size.width * 0.12,
+                  top: size.height * (0.97 - 0.035),
+                  child: Text(
+                    "Image",
+                    style: TextStyle(
+                      fontFamily: 'RoundLight',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * 0.99,
+                  left: size.height * 0.07,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: customPurple),
+                        borderRadius: BorderRadius.circular(100)),
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      elevation: 0,
+                      fillColor: Color(0xFFF3F3F3),
+                      child: Icon(
+                        Icons.upload_outlined,
+                        size: 60.0,
+                      ),
+                      padding: EdgeInsets.all(15.0),
+                      shape: CircleBorder(),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * 1.2,
+                  left: size.width * 0.12,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+                    height: size.height * 0.25,
+                    width: size.width * 0.77,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF3F3F3),
+                      borderRadius: BorderRadius.circular(29),
+                      border: Border.all(color: customPurple),
+                    ),
+                    child: TextFormField(
+                      controller: _aboutYController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 0,
+                          vertical: size.height * 0.014,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontFamily: 'RoundLight',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 17,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: size.width * 0.12,
+                  top: size.height * (1.2 - 0.035),
+                  child: Text(
+                    "About yourself",
+                    style: TextStyle(
+                      fontFamily: 'RoundLight',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * 1.48,
+                  child: FlatButton(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10, horizontal: size.width * 0.07),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => WelcomeScreen()));
+                      },
+                      color: customPurple,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text(
+                        "Create Account",
+                        style: TextStyle(
+                            fontFamily: 'RoundLight',
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20,
+                            color: Colors.white),
+                      )),
                 ),
               ],
             ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
