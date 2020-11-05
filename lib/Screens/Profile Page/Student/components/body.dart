15e2fe39c446feeded500b4611997cf4adbf63dc
Marin Fabijanic <mf51426@fer.hr>
2020-11-05 17:51:49 +0100

    Merge prep

---
+++ lib/Screens/Profile Page/Student/components/body.dart
@@ -0,0 +1,128 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:jamesbondi/constants.dart';
+import 'package:jamesbondi/components/getUser.dart';
+
+class Body extends StatefulWidget {
+  final User loggedUser;
+
+  Body(this.loggedUser);
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    final User loggedUser = widget.loggedUser;
+    Map<String, dynamic> userInfo;
+    getUserInfo(loggedUser.email).then((value) => userInfo);
+    Size size = MediaQuery.of(context).size;
+    return FutureBuilder(
+        future: getUserInfo(loggedUser.email),
+        builder: (BuildContext context,
+            AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          Widget children;
+          if (snapshot.hasData) {
+            children = SingleChildScrollView(
+              child: Container(
+                height: size.height,
+                width: double.infinity,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    // Top artwork
+                    Positioned(
+                      top: 0,
+                      child: Image.asset(
+                        'assets/images/top_part_profile.png',
+                        width: size.width,
+                      ),
+                    ),
+
+                    //My Profile
+                    Positioned(
+                      top: size.height * 0.05,
+                      left: size.width * 0.05,
+                      child: Text(
+                        "My Profile",
+                        style: TextStyle(
+                            // decoration: TextDecoration.underline,
+                            fontFamily: 'Quiglet',
+                            fontWeight: FontWeight.normal,
+                            fontSize: 25,
+                            color: Colors.white),
+                      ),
+                    ),
+
+                    // Edit icon
+                    Positioned(
+                      top: size.height * 0.05,
+                      right: size.width * 0.05,
+                      child: Image.asset(
+                        'assets/images/pencil.png',
+                        height: size.height * 0.04,
+                      ),
+                    ),
+
+                    // Profile photo
+                    Positioned(
+                      top: size.height * 0.23,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: customPurple),
+                            borderRadius: BorderRadius.circular(100)),
+                        child: RawMaterialButton(
+                          onPressed: () {},
+                          elevation: 0,
+                          fillColor: Color(0xFFF3F3F3),
+                          child: Icon(
+                            Icons.person,
+                            size: 100.0,
+                            color: Colors.black54,
+                          ),
+                          padding: EdgeInsets.all(15.0),
+                          shape: CircleBorder(),
+                        ),
+                      ),
+                    ),
+
+                    // First and Last Name
+                    Positioned(
+                      top: size.height * 0.43,
+                      child: Text(
+                        snapshot.data['firstName'] +
+                            ' ' +
+                            snapshot.data['lastName'],
+                        style: TextStyle(
+                          fontFamily: 'RoundLight',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 22,
+                          color: customPurple,
+                        ),
+                      ),
+                    ),
+
+                    // Type of Person
+                    Positioned(
+                      top: size.height * 0.47,
+                      child: Text(
+                        'Student',
+                        style: TextStyle(
+                          fontFamily: 'RoundLight',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            children = Container();
+          }
+          return children;
+        });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:jamesbondi/constants.dart';
+import 'package:jamesbondi/Screens/Welcome/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/rendering.dart';
-import 'signup.dart';
-import 'signin.dart';
 
-void main() {
-  runApp(App());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
 }
 
-//Test branch
-//Dodik commit :(
-
-class App extends StatefulWidget {
-  _AppState createState() => _AppState();
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-class _AppState extends State<App> {
-  // Set default `_initialized` and `_error` state to false
+class _HomeState extends State<Home> {
   bool _initialized = false;
   bool _error = false;
 
@@ -45,348 +49,15 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    // Show error message if initialization failed
-    if (_error) {
-      //return SomethingWentWrong();
-    }
-
-    // Show a loader until FlutterFire is initialized
-    if (!_initialized) {
-      //return Loading();
-    }
-
-    return MyApp();
-  }
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
-  @override
-  Widget build(BuildContext context) {
-    var materialApp = MaterialApp(
-        title: 'Main',
-        home: Scaffold(
-            body: Container(
-          color: Color(0xEEEEEE),
-          padding: const EdgeInsets.only(
-            left: 10.0,
-            top: 300.0,
-          ),
-          child: Column(
-            children: [
-              Center(
-                child: Container(
-                  //color: Colors.blueGrey[100],
-                  child: Container(
-                    child: Text("Mail"),
-                  ),
-                ),
-              ),
-
-              Container(
-                width: 250.0,
-                height: 30.0,
-                child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xF3F3F3),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.purple[800]),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.purple[800]),
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide: BorderSide(
-                            width: 1,
-                          )),
-                      errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide:
-                              BorderSide(width: 1, color: Colors.black)),
-                      focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide: BorderSide(width: 5, color: Colors.red)),
-                      hintText: "mail",
-                      hintStyle:
-                          TextStyle(fontSize: 16, color: Color(0xFFB3B1B1)),
-                      //errorText: 'username is already taken',
-                    ),
-                    textAlignVertical: TextAlignVertical.bottom,
-                    textAlign: TextAlign.left),
-              ),
-
-              //drugi dio
-              Center(
-                child: Container(
-                  padding: const EdgeInsets.only(
-                    top: 30.0,
-                  ),
-                  //color: Colors.blueGrey[1000],
-                  child: Container(
-                    child: Text("Username"),
-                  ),
-                ),
-              ),
-              Container(
-                width: 250.0,
-                height: 30.0,
-                child: new TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xF3F3F3),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.purple[800]),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.purple[800]),
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide: BorderSide(
-                            width: 1,
-                          )),
-                      errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide:
-                              BorderSide(width: 1, color: Colors.black)),
-                      focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide: BorderSide(width: 5, color: Colors.red)),
-                      hintText: "username",
-                      hintStyle:
-                          TextStyle(fontSize: 16, color: Color(0xFFB3B1B1)),
-                      //errorText: 'username is already taken',
-                    ),
-                    textAlignVertical: TextAlignVertical.bottom,
-                    textAlign: TextAlign.left),
-              ),
-
-              //treci dio
-              Center(
-                child: Container(
-                  padding: const EdgeInsets.only(
-                    top: 30.0,
-                  ),
-                  //color: Colors.blueGrey[1000],
-                  child: Container(
-                    child: Text("Password"),
-                  ),
-                ),
-              ),
-
-              Container(
-                width: 250.0,
-                height: 30.0,
-                child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xF3F3F3),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.purple[800]),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.purple[800]),
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide: BorderSide(
-                            width: 1,
-                          )),
-                      errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide:
-                              BorderSide(width: 1, color: Colors.black)),
-                      focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          borderSide: BorderSide(width: 5, color: Colors.red)),
-                      hintText: "password",
-                      hintStyle:
-                          TextStyle(fontSize: 16, color: Color(0xFFB3B1B1)),
-                      //errorText: 'username is already taken',
-                    ),
-                    textAlignVertical: TextAlignVertical.bottom,
-                    textAlign: TextAlign.left),
-              ),
-              //button
-              Container(
-                  padding: const EdgeInsets.only(
-                    top: 50.0,
-                  ),
-                  child: OutlineButton(
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0),
-                      ),
-                      onPressed: () {},
-                      textColor: Colors.white,
-                      //padding: const EdgeInsets.all(0.0),
-                      child: Container(
-                          width: 200.0,
-                          height: 50.0,
-                          decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: <Color>[
-                                Color(0xFF8E24AA),
-                                Color(0xFFAB47BC),
-                                Color(0xFFBA68C8),
-                              ],
-                            ),
-                          ),
-                          padding: const EdgeInsets.all(5.0),
-                          child: Center(
-                            child: const Text(
-                              'Sign up',
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.center,
-                            ),
-                          ))))
-            ],
-          ),
-        )));
-
-    return materialApp;
-  }
-}
-
-BoxDecoration myBoxDecoration() {
-  return BoxDecoration(
-    border: Border.all(
-      color: Colors.purple[800],
-      width: 5.0,
-    ),
-  );
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void signOutButton() async {
-    final User user = auth.currentUser;
-    if (user == null) {
-      signed = false;
-      return;
-    }
-    await auth.signOut();
-    final String uid = user.uid;
-    signed = false;
-    Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text(uid + ' has successfully signed out.'),
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome',
+      theme: ThemeData(
+        backgroundColor: bgColor,
+        scaffoldBackgroundColor: bgColor,
       ),
-      body: Builder(builder: (BuildContext context) {
-        return ListView(
-          scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.all(16),
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text(
-                  'You are signed ',
-                ),
-                Text(
-                  signed ? "IN" : "OUT",
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Container(
-                  height: 300,
-                  child: RegisterEmailSection(),
-                ),
-                Container(
-                  height: 300,
-                  child: EmailPasswordForm(),
-                ),
-              ],
-            ),
-          ],
-        );
-      }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: signOutButton,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-double _volume = 0.0; //volume
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        IconButton(
-          icon: Icon(Icons.volume_up),
-          tooltip: 'Increase by one volume',
-          onPressed: () {
-            setState(() {
-              _volume += 1;
-            });
-          },
-        ),
-        Text('Volume : $_volume')
-      ],
-    );
-  }
-}
-
-class MyStatefulWdget extends StatefulWidget {
-  @override
-  _MyStatefulWdgetState createState() => _MyStatefulWdgetState();
-}
-
-class _MyStatefulWdgetState extends State<MyStatefulWdget> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        IconButton(
-          icon: Icon(Icons.volume_up),
-          tooltip: 'Increase by one volume',
-          onPressed: () {
-            setState(() {
-              _volume += 1;
-            });
-          },
-        ),
-        Text('Volume : $_volume')
-      ],
+//      home: WelcomeScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
